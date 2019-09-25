ffecbdabb79a343f413d5936d563f17869133234
Vicky Banks <vicky.banks@bbc.co.uk>
2019-09-25 14:28:42 +0100

    Script to join time viewed with VMF and investigate

---
+++ VFM_timeSpentBBC.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(DBI)
+library(dbplyr)
+library("RSQLite")
+library(knitr)
+library(gridExtra)
+library(grid)
+library(ggplot2)
+library(reshape2)
+library(lubridate)
+library(readr)
+
+
