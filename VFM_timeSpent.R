2849ada7149e5701523eeb556e5286f6d2f71b6b
Vicky Banks <vicky.banks@bbc.co.uk>
2019-09-27 15:08:05 +0100

    Corrected code and added graphs with lm line

--- VFM_timeSpent.R
+++ VFM_timeSpent.R
@@ -12,6 +12,7 @@ library(readr)
 library(tidyr)
 library(stringr)
 library(data.table)
+library(ggpmisc)
 
 streamLabels <- read.csv("D:\\Projects\\VMF_Regression\\StreamLabels.csv", header = TRUE)
 
@@ -19,35 +20,113 @@ streamLabels <- read.csv("D:\\Projects\\VMF_Regression\\StreamLabels.csv", heade
 #### bring in data from sqlite database
 dbPath <- ("D:\\Projects\\CMMData.db")
 con <- dbConnect(RSQLite::SQLite(), dbname=dbPath)
-vfm <- collect(tbl(con, 'panellstsVMF'))
-audience<- collect(tbl(con, 'audienceData12Weeks_incWeb'))
+vfm1 <- collect(tbl(con, 'panellstsVMF'))
+temp <- tbl(con, 'audienceData12Weeks_incWeb')
+audience<- collect(filter(temp, WEEK <= 8,
+                  WEEK >= 5))
 dbDisconnect(con)
 
-audienceBBC<- inner_join(audience, streamLabels, by = 'STREAM_LABEL')
-audienceBBC<- audienceBBC %>% filter(startsWith(audienceBBC$INDIVIDUAL_ID, 'I') |startsWith(audienceBBC$INDIVIDUAL_ID, 'H') )
+audienceBBC<- audience %>% 
+  filter(startsWith(audience$INDIVIDUAL_ID, 'I') |startsWith(audience$INDIVIDUAL_ID, 'H') )%>% 
+  mutate(ID = substr(INDIVIDUAL_ID, 3,11)) %>% 
+  select(-INDIVIDUAL_ID) %>%
+  distinct()
+audienceBBC<- inner_join(audienceBBC, streamLabels, by = 'STREAM_LABEL')
 
-radio <- audienceBBC %>% filter(TYPE == 'RADIO') %>% 
-  group_by(INDIVIDUAL_ID, WEEK) %>% 
+vfm <- vfm1 %>%mutate(ID = substr(INDIVIDUAL_ID, 3,11)) %>% 
+  select(-INDIVIDUAL_ID) %>% 
+  filter(AGERAW <35) %>% 
+  select(ID, BBC_VMF) %>% 
+  distinct()
+
+
+
+
+BBC_VFM<- inner_join(audienceBBC, vfm, by = 'ID')
+
+allBBC <- BBC_VFM %>% group_by(ID, WEEK) %>%
   summarise(totalDur = sum(as.numeric(hms(DURATION)))) %>%
-  group_by(INDIVIDUAL_ID) %>%
-  summarise(avgDuration = mean(totalDur))
+  group_by(ID) %>%
+ summarise(avgDuration = round(mean(totalDur)/(7*3600),1))
 
-tv <- audienceBBC %>% filter(TYPE == 'TV') %>% 
-  group_by(INDIVIDUAL_ID, WEEK) %>% 
+allBBC<- inner_join(allBBC, vfm) 
+
+my.formula <- y ~ x
+ggplot(data = allBBC, mapping = aes(x = avgDuration, y = BBC_VMF)) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = "lm", se=FALSE, color="black", formula = my.formula) +
+  stat_poly_eq(formula = my.formula, 
+               aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
+               parse = TRUE)+
+  theme_classic() +
+  labs(title = "BBC VFM vs. Avg Time Spent", x = "Average Hours With BBC Per Day", y = "BBC VFM",
+       subtitle = "16-34", caption = "Compass: W5-8 2019")
+
+
+
+
+write.csv(allBBC, file = "D:\\Projects\\VMF_Regression\\data\\allBBC.csv", row.names = FALSE)
+
+
+tooManyHours<- allBBC %>%filter(avgDuration> 24)
+
+
+view(audienceBBC %>% filter(ID =='1007677_1'))
+
+
+
+radio <- BBC_VFM %>% filter(TYPE == 'RADIO') %>%
+ group_by(ID, WEEK) %>%
   summarise(totalDur = sum(as.numeric(hms(DURATION)))) %>%
-  group_by(INDIVIDUAL_ID) %>%
-  summarise(avgDuration = mean(totalDur))
+  group_by(ID) %>%
+  summarise(avgDuration = round(mean(totalDur)/(7*3600),1))
+radio <- inner_join(radio, vfm)
 
-online <- audienceBBC %>% filter(TYPE == 'WEB') %>% 
-  group_by(INDIVIDUAL_ID, WEEK) %>% 
+tv <- BBC_VFM %>% filter(TYPE == 'TV') %>%
+  group_by(ID, WEEK) %>%
   summarise(totalDur = sum(as.numeric(hms(DURATION)))) %>%
-  group_by(INDIVIDUAL_ID) %>%
-  summarise(avgDuration = mean(totalDur))
+  group_by(ID) %>%
+  summarise(avgDuration = round(mean(totalDur)/(7*3600),1))
+tv <- inner_join(tv, vfm)
 
-radioVFM <-inner_join(radio, vfm) 
-tvVFM <-inner_join(tv, vfm )
-onlineVFM <-inner_join(online, vfm ) 
+online <- BBC_VFM %>% filter(TYPE == 'WEB') %>%
+  group_by(ID, WEEK) %>%
+  summarise(totalDur = sum(as.numeric(hms(DURATION)))) %>%
+  group_by(ID) %>%
+  summarise(avgDuration = round(mean(totalDur)/(7*3600),1))
+online <- inner_join(online, vfm)
 
-write.csv(radioVFM, file = "D:\\Projects\\VMF_Regression\\data\\radioVFM.csv", row.names = FALSE)
-write.csv(tvVFM, file = "D:\\Projects\\VMF_Regression\\data\\tvVFM.csv", row.names = FALSE)
-write.csv(onlineVFM, file = "D:\\Projects\\VMF_Regression\\data\\onlineVFM.csv", row.names = FALSE)
+radioGraph<- ggplot(data = radio, mapping = aes(x = avgDuration, y = BBC_VMF)) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = "lm", se=FALSE, color="black", formula = my.formula) +
+  stat_poly_eq(formula = my.formula, 
+               aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
+               parse = TRUE)+
+  theme_classic() +
+  labs(title = "BBC VFM vs. Avg Time Spent RADIO", x = "Average Hours With BBC Per Day", y = "BBC VFM",
+       subtitle = "16-34", caption = "Compass: W5-8 2019")
+radioGraph
+
+
+tvGraph<- ggplot(data = tv, mapping = aes(x = avgDuration, y = BBC_VMF)) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = "lm", se=FALSE, color="black", formula = my.formula) +
+  stat_poly_eq(formula = my.formula, 
+               aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
+               parse = TRUE)+
+  theme_classic() +
+  labs(title = "BBC VFM vs. Avg Time Spent TV", x = "Average Hours With BBC Per Day", y = "BBC VFM",
+       subtitle = "16-34", caption = "Compass: W5-8 2019")
+tvGraph
+
+onlineGraph<-ggplot(data = online, mapping = aes(x = avgDuration, y = BBC_VMF)) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = "lm", se=FALSE, color="black", formula = my.formula) +
+  stat_poly_eq(formula = my.formula,
+               aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+               parse = TRUE)+
+  theme_classic() +
+  labs(title = "BBC VFM vs. Avg Time Spent ONLINE", x = "Average Hours With BBC Per Day", y = "BBC VFM",
+       subtitle = "16-34", caption = "Compass: W5-8 2019")
+onlineGraph
+
