3eb757eee1d564bad16eb2b107aee1eaf735211f
Vicky Banks <vicky.banks@bbc.co.uk>
2019-10-09 19:06:58 +0100

    Added animaltion of model and heat map of results

--- kmeans/kMeansClustering.R
+++ kmeans/kMeansClustering.R
@@ -1,5 +1,8 @@
-
-
+library(ggplot2)
+library(animation)
+library(tidyr)
+library(RColorBrewer)
+##### Data 1. Time Spent on the TV Channels ######
 data <-read.csv("D:\\Projects\\VMF_Regression\\data\\Time Spent Channels\\timeSpentChannel.csv",header=T)
 summary(data)
 
@@ -18,7 +21,6 @@ for(col in 1:ncol(timeSpent)){
     if(timeSpent[row,col] > topLimit){timeSpent[row,col]<- topLimit}
   }
 }
-
 summary(timeSpent)
 
 ######## Using the elbow plot to find a good number of clusters ####
@@ -31,7 +33,7 @@ avg_wssCluster<- integer(length(clusterNums)) #make an empty list to hold result
 for(v in clusterNums){
   wssCluster_v <- integer(tries) 
   for(i in 1:tries){
-    k.temp <- kmeans(timeSpent[,c(2:3)], centers = v) 
+    k.temp <- kmeans(timeSpent[,c(5:8)], centers = v) 
     wssCluster_v[i] <- k.temp$tot.withinss 
   }
   avg_wssCluster[v-1] <-mean(wssCluster_v)
@@ -43,9 +45,25 @@ plot(clusterNums,avg_wssCluster, type="b", main="Total Within SS by Various K",
      xlab="Value of K")
 ## Something around 4 or 5 looks the best
 
-k<- kmeans(timeSpent[,c(2:3)], centers = 5)
+k<- kmeans.ani(timeSpent[,c(5:8)], centers = 5)
 k$centers 
 table(k$cluster)
+summary(timeSpent[,c(2:4)])
+
 
 ### visualise results of kmeans
-## interpret results
+cluster <- c(1:5)
+centerDF <- data.frame(cluster, k$center)
+
+centerReshape <- gather(centerDF, channels, values, CBBC:PARLIAMENT)
+head(centerReshape)
+
+heatMapPalette <-colorRampPalette(rev(brewer.pal(10, 'Spectral')),space='Lab')## colour palette
+
+#heatMapPalette <-colorRampPalette(rev(brewer.pal(10, 'RdYlGn')),space='Lab')## colour palette
+ggplot(data = centerReshape, aes(x = channels, y = cluster, fill = values)) +
+  scale_y_continuous(breaks = seq(1, 5, by = 1)) +
+  geom_tile() +
+  coord_equal() +
+  scale_fill_gradientn(colours = heatMapPalette(90)) +
+  theme_classic()
