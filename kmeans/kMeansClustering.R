d719c5b20f4e76ac80da66e580daf66cdc73f318
Vicky Banks <vicky.banks@bbc.co.uk>
2019-10-10 15:24:04 +0100

    Update kMeansClustering.R

--- kmeans/kMeansClustering.R
+++ kmeans/kMeansClustering.R
@@ -2,27 +2,44 @@ library(ggplot2)
 library(animation)
 library(tidyr)
 library(RColorBrewer)
-##### Data 1. Time Spent on the TV Channels ######
-data <-read.csv("D:\\Projects\\VMF_Regression\\data\\Time Spent Channels\\timeSpentChannel.csv",header=T)
-summary(data)
+library(fmsb)
+################### Data 1. Time Spent on the TV Channels #######################
+data <-read.csv("D:\\Projects\\VMF_Regression\\data\\timeSpentTV.csv",header=T)
+#summary(data)
 
-timeSpent<- data[,c(7:14)]##only select columns needed
+timeSpent<- data[,c(2:9)]##only select columns needed
 
-### set anything above the 95th percentile to the 95th percentile value
-trimData <- function(x){
-  topLimit <<- quantile( x, c(0.95 ))
-  print(topLimit)
-  return(topLimit)
-}
+
+# ### set anything above the 95th percentile to the 95th percentile value
+# trimData <- function(x){
+#   topLimit <<- quantile( x, c(0.95 ))
+#   print(topLimit)
+#   return(topLimit)
+# }
+# 
+# for(col in 1:ncol(timeSpent)){
+#   print(col)
+#   trimData(timeSpent[,col])
+#   for(row in 1:nrow(timeSpent)){ 
+#     if(timeSpent[row,col] > topLimit){timeSpent[row,col]<- topLimit}
+#   }
+# }
+
+##turn into percentile values
 for(col in 1:ncol(timeSpent)){
-  print(col)
-  trimData(timeSpent[,col])
-  for(row in 1:nrow(timeSpent)){ 
-    if(timeSpent[row,col] > topLimit){timeSpent[row,col]<- topLimit}
-  }
+  timeSpent[,col]<- percentile( timeSpent[,col])
 }
 summary(timeSpent)
-
+### set the lowest percentile to almost zero to make the results easier to understand
+# for(row in 1:nrow(timeSpent)){
+#   if(timeSpent$ALBA[row] == 97.00){timeSpent$ALBA[row] = 0.01}
+#   if(timeSpent$CBBC[row] == 92.00){timeSpent$CBBC[row] = 0.01}
+#   if(timeSpent$CBEEBIES[row] == 85.00){timeSpent$CBEEBIES[row] = 0.01}
+#   if(timeSpent$FOUR[row] == 67.00){timeSpent$FOUR[row] = 0.01}
+#   if(timeSpent$NEWS[row] == 70.00){timeSpent$NEWS[row] = 0.01}
+#   if(timeSpent$PARLIAMENT[row] == 94.00){timeSpent$PARLIAMENT[row] = 0.01}
+#   if(timeSpent$TWO[row] == 97.00){timeSpent$TWO[row] = 0.01}
+# }
 ######## Using the elbow plot to find a good number of clusters ####
 clusterNums <-2:20
 tries <- 100
@@ -33,7 +50,7 @@ avg_wssCluster<- integer(length(clusterNums)) #make an empty list to hold result
 for(v in clusterNums){
   wssCluster_v <- integer(tries) 
   for(i in 1:tries){
-    k.temp <- kmeans(timeSpent[,c(5:8)], centers = v) 
+    k.temp <- kmeans(timeSpent[,c(1:8)], centers = v) 
     wssCluster_v[i] <- k.temp$tot.withinss 
   }
   avg_wssCluster[v-1] <-mean(wssCluster_v)
@@ -45,7 +62,8 @@ plot(clusterNums,avg_wssCluster, type="b", main="Total Within SS by Various K",
      xlab="Value of K")
 ## Something around 4 or 5 looks the best
 
-k<- kmeans.ani(timeSpent[,c(5:8)], centers = 5)
+k<- kmeans.ani(timeSpent[,c(1:8)], centers = 4)
+#k<- kmeans(timeSpent[,c(2:8)], centers = 5)
 k$centers 
 table(k$cluster)
 summary(timeSpent[,c(2:4)])
@@ -55,7 +73,7 @@ summary(timeSpent[,c(2:4)])
 cluster <- c(1:5)
 centerDF <- data.frame(cluster, k$center)
 
-centerReshape <- gather(centerDF, channels, values, CBBC:PARLIAMENT)
+centerReshape <- gather(centerDF, channels, values, ALBA:TWO)
 head(centerReshape)
 
 heatMapPalette <-colorRampPalette(rev(brewer.pal(10, 'Spectral')),space='Lab')## colour palette
@@ -67,3 +85,6 @@ ggplot(data = centerReshape, aes(x = channels, y = cluster, fill = values)) +
   coord_equal() +
   scale_fill_gradientn(colours = heatMapPalette(90)) +
   theme_classic()
+##################################################################################
+
+################### Data 2. Platform visited per week #######################
